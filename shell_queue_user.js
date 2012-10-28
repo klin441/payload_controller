2d36b4a305b66606b30ea0ab61bf85029e5c4c72
klin441 <kailinchau@hotmail.com>
2012-10-28 22:16:45 +1100

    asdf

--- shell_queue_user.js
+++ shell_queue_user.js
@@ -9,7 +9,7 @@ var response2 = new events.EventEmitter();
 
 myQueue.add('echo lol', response);
 myQueue.add('echo cool', response);
-myQueue.add('sleep 5', response);
+myQueue.add('sleep 3', response);
 myQueue.add('echo still awake?', response); 
 myQueue.add('echo 1 >> ./tmp', response);
 myQueue.add('echo 2 >> ./tmp', response);
@@ -17,7 +17,7 @@ myQueue.add('echo 1st queue 1; sleep 1', response);
 myQueue.add('echo 1st queue 2; sleep 1', response);
 myQueue.add('echo 1st queue 3; sleep 1', response);
 
-myQueue2.add('echo 2nd queue 1; sleep 1', response2);
+myQueue2.add('echo 2nd queue 1; sleep 2', response2);
 myQueue2.add('echo 2nd queue 2; sleep 1', response2);
 myQueue2.add('echo 2nd queue 3; sleep 1', response2);
 myQueue2.add('echo 2nd queue 4; sleep 1', response2);
@@ -33,10 +33,10 @@ setTimeout(function() {
 
 // change listeners to one time only 
 
-response.on('complete', function(stdout, error) {
-    console.log('complete event raised - stdout: ' + stdout);
+response.on('complete', function(stdout, error, cmd) {
+    console.log('completed - stdout: ' + stdout + ', cmd: ' + cmd + Date(), "\n");
 });
 
-response2.on('complete', function(stdout, error) {
-    console.log('2nd queue complete even raised - stdout: ' + stdout);
+response2.on('complete', function(stdout, error, cmd) {
+    console.log('2nd queue complete even raised - stdout: ' +  stdout + ', cmd: ' + cmd + Date(), "\n");
 });
