128d817211f1c9b77e2052cfe3ff454f199d0bb1
klin441 <kailinchau@hotmail.com>
2012-10-28 22:37:38 +1100

    replaced eventemitter with callback

--- shell_queue.js
+++ shell_queue.js
@@ -3,48 +3,50 @@ var events = require('events');
 var exec = require("child_process").exec;
 
 
-// each added task gives ShellQueue one response listener
-
-function ShellQueue() {
+function ShellQueue(name) {
     var self = this;
-    
+    self.name = name; 		// for debugging 
     self.queue = [];		// a queue of shell tasks
-    self.isIdle = 1;			// allows for the required blocking execution
+    self.isIdle = 1;		// allows for the required blocking execution
 }
 sys.inherits(ShellQueue, events.EventEmitter);
 
 // change to callbacks instead of listeners
 
-ShellQueue.prototype.add = function(shell_cmd_string, response) {
+ShellQueue.prototype.add = function(shell_cmd_string, callback) {
     var self = this;
-    var object = {};
-    object.cmd = shell_cmd_string;
-    object.response = response;
+    var task = {};
+    task.cmd = shell_cmd_string;
+    task.callback = callback;
     
-    self.queue.push(object);
+    self.queue.push(task);
     if (self.isIdle) {
         self.isIdle = 0;
         self._execute();
     }    
+    
+    console.log('new task added');
 }
 
 // an internal function.
 ShellQueue.prototype._execute = function() {
     var self = this;
-    var object = self.queue.shift();
+    var task = self.queue.shift();
     // want blocking execution on the same queue 
-    //console.log('about to _execute ' + object.cmd);
-    exec(object.cmd, function(error, stdout, stderr) {
+    //console.log('about to _execute ' + task.cmd);
+    exec(task.cmd, function(error, stdout, stderr) {
         
-        // tell outside world one task just completed
-        object.response.emit('complete', stdout, error, object.cmd);
-                
+        if (task.callback) {
+            task.callback(error, stdout, stderr, task.cmd);
+        }
+        
         // keep processing the queue
         if (self.queue.length > 0) {
             self._execute();
         } else {
             // put queue into an idle state
             self.isIdle = 1;
+            console.log('queue', self.name, 'is idle');
         }
     });
 }
