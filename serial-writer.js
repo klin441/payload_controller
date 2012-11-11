69ed9b7533019698bd0c2ea552f1cd35e1cccad3
klin441 <kailinchau@hotmail.com>
2012-11-11 23:20:05 +1100

    asdf

--- serial-writer.js
+++ serial-writer.js
@@ -4,16 +4,15 @@ var exec = require('child_process').exec;
 
 
 var baudrate = 9600;
-
-sp = new SerialPort("/dev/ttyO2", {
-    parser: serialport.parsers.readline("\n"),
-    baudrate: baudrate,
-});
-
 var stdin = process.openStdin();
 
 exec('echo 1 > /sys/kernel/debug/omap_mux/spi0_sclk', function() {
     exec('echo 21 >/sys/kernel/debug/omap_mux/spi0_d0', function() {
+        sp = new SerialPort("/dev/ttyO2", {
+			parser: serialport.parsers.readline("\n"),
+			baudrate: baudrate,
+		});
+        
         stdin.on('data', function(chunk) { 
             console.log("Got chunk: " + chunk);
             sp.write(chunk);
