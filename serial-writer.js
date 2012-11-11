f977a80b11bdd191fd2707d76d580ef9445e4cfe
klin441 <kailinchau@hotmail.com>
2012-11-11 23:36:28 +1100

    asdf

--- serial-writer.js
+++ serial-writer.js
@@ -8,6 +8,7 @@ var stdin = process.openStdin();
 exec('echo 1 > /sys/kernel/debug/omap_mux/spi0_sclk', function() {
     exec('echo 21 > /sys/kernel/debug/omap_mux/spi0_d0', function() {
         var sp = new SerialPort("/dev/ttyO2", {
+            parser: serialport.parsers.raw,
 			baudrate: 9600,
 		});
         
@@ -15,6 +16,10 @@ exec('echo 1 > /sys/kernel/debug/omap_mux/spi0_sclk', function() {
             console.log("Got chunk: " + chunk);
             sp.write(chunk+"\n");
         });
+        
+        sp.on("data", function (data) {
+		    sys.puts("here: "+data);
+		});
     });
 });
 
